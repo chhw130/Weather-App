9f314bc728dae7af00e6928ebd591504a5fe4b97
chhw130 <527coco@naver.com>
2023-10-10 19:13:41 +0900

    first Setting

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from "@/App.vue"
+import router from './router'
+import 'ant-design-vue/dist/reset.css';
+import Antd from 'ant-design-vue'
+
+const app = createApp(App)
+
+app.use(router).use(Antd)
+
+app.mount('#app')
